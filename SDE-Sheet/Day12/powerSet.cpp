b69b5d68b37d09abcc79bf5f33260613426d23c9
Dhanushkumar <dhanushkandasamy13@gmail.com>
2021-10-20 15:28:16 +0530

    Commit

---
+++ SDE-Sheet/Day12/powerSet.cpp
@@ -0,0 +1,22 @@
+//GFG soln
+
+class Solution{
+	public:
+		vector<string> AllPossibleStrings(string s){
+		    // Code here
+		    int n = s.size(); 
+		    vector<string> ans; 
+		    for(int num = 0; num < (1 << n); num++) {
+		        string sub = ""; 
+		        for(int i = 0;i<n;i++) {
+		            if(num & (1<<i)) {
+		                sub += s[i];
+		            }
+		        }
+		        if(sub.size() > 0)
+		            ans.push_back(sub);
+		    }
+		    sort(ans.begin(), ans.end()); 
+		    return ans;
+		}
+};
