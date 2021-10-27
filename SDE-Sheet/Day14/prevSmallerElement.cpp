43112dca00a1eb305d0f9b026ff596c62854d1c5
Dhanushkumar <dhanushkandasamy13@gmail.com>
2021-10-27 21:19:27 +0530

    Commit

--- SDE-Sheet/Day14/prevSmallerElement.cpp
+++ SDE-Sheet/Day14/prevSmallerElement.cpp
@@ -1,21 +1,21 @@
 //InterviewBit
 
-// vector<int> Solution::prevSmaller(vector<int> &A) {
-//     stack<int> s;
-//     int n = A.size();
-//     vector<int> ans(n);
-//     for(int i = 0; i < n; i++){
-//         while(!s.empty() && A[i] <= s.top()) s.pop();
-//         if(s.empty()) ans[i] = -1;
-//         else{
-//             ans[i] = s.top();
-//         }
-//         s.push(A[i]);
-//     }
-//     return ans;
-// }
+vector<int> Solution::prevSmaller(vector<int> &A) {
+    stack<int> s;
+    int n = A.size();
+    vector<int> ans(n);
+    for(int i = 0; i < n; i++){
+        while(!s.empty() && A[i] <= s.top()) s.pop();
+        if(s.empty()) ans[i] = -1;
+        else{
+            ans[i] = s.top();
+        }
+        s.push(A[i]);
+    }
+    return ans;
+}
 
-
+// Entire Code
 #include<bits/stdc++.h>
 using namespace std;
 void prevSmallerElement(vector<int> v, vector<int> &ans){
