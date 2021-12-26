5642cf3b3d9740bcc1ea4b1f758c69f0fce4c678
Dhanushkumar <48850685+Dhanush-dk@users.noreply.github.com>
2021-12-26 14:54:17 +0530

    Added

---
+++ SDE-Sheet/Day3-Array/rotateMatrix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int size = matrix.size();
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = i+1; j < matrix[0].size(); j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = 0; j < matrix[0].size()/2 ; j++){
+                swap(matrix[i][j], matrix[i][size-j-1]);
+            }
+        }
+    }
+};
