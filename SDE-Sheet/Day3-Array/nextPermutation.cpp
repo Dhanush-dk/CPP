bd6490b38fdd11896734fe4c141758e1efba77b1
Dhanushkumar <48850685+Dhanush-dk@users.noreply.github.com>
2021-12-26 12:44:30 +0530

    added

---
+++ SDE-Sheet/Day3-Array/nextPermutation.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i;
+        for(i = n-2; i >= 0; i--){
+            if(nums[i] < nums[i+1]){
+                break;
+            }
+        }
+        if(i < 0){
+            reverse(nums.begin(), nums.end());
+        }
+        else{
+            for(int j = n-1; j > i; j--){
+                if(nums[j] > nums[i]){
+                    swap(nums[j], nums[i]);
+                    reverse(nums.begin() + i + 1, nums.end());
+                    break;
+                }
+            }
+        }
+            
+    }
+};
