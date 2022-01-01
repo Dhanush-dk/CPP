d2524b540e29cb70d2d81cdea75ff05ef3693171
Dhanushkumar <48850685+Dhanush-dk@users.noreply.github.com>
2022-01-01 22:41:23 +0530

    updated

--- SDE-Sheet/Day17-BinaryTree/inorderTraversal.cpp
+++ SDE-Sheet/Day17-BinaryTree/inorderTraversal.cpp
@@ -1,3 +1,4 @@
+// Recursive soln
 class Solution {
 public:
     void bfs(TreeNode* root, vector<int> &ans){
@@ -11,5 +12,55 @@ public:
         if(root == NULL) return ans;
         bfs(root, ans);
         return ans;
+    }
+};
+
+
+//Iterative soln 1
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(!root) return ans;
+        stack<TreeNode*> st;
+        TreeNode *currNode = root;
+        while(currNode || !st.empty()){
+            while(currNode != NULL){
+                st.push(currNode);
+                currNode = currNode->left;
+            }
+            currNode = st.top();
+            st.pop();
+            ans.push_back(currNode->val);
+            currNode = currNode->right;
+        }
+        return ans;
+    }
+};
+
+
+// Iterative soln 2
+class Solution {
+
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*> s; 
+        TreeNode* node = root;
+        vector<int> ans; 
+        while(true) {
+            if(node != NULL) {
+                st.push(node); 
+                node = node->left; 
+            }
+            else {
+    
+                if(st.empty() == true) break; 
+                node = st.top(); 
+                st.pop(); 
+                ans.push_back(node->val); 
+                node = node->right; 
+            }  
+        }
+        return ans; 
     }
 };
