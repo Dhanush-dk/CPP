637d3aba12520781f66a7a9347fcb0446acbf8ba
Dhanushkumar <48850685+Dhanush-dk@users.noreply.github.com>
2022-01-03 14:22:20 +0530

    Created

---
+++ SDE-Sheet/Day15-Strings/longestCommonPrefix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string res = strs[0];
+        for(int i = 0; i < strs.size(); i++){
+            string s = strs[i];
+            int j = 0;
+            for(; j < strs[i].size(); j++){
+                if(res[j] != s[j]) break;
+            }
+            res = res.substr(0, j);
+        }
+        return res;
+    }
+};
