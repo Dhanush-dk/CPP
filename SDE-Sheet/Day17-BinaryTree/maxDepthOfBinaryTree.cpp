f9ea7423998e84a788db03edae0980223fc4d9b7
Dhanushkumar <48850685+Dhanush-dk@users.noreply.github.com>
2022-01-08 16:34:26 +0530

    Updated

--- SDE-Sheet/Day17-BinaryTree/maxDepthOfBinaryTree.cpp
+++ SDE-Sheet/Day17-BinaryTree/maxDepthOfBinaryTree.cpp
@@ -9,3 +9,25 @@ public:
         return 1 + max(l,r);
     }
 };
+
+// Iterative soln using level order traversal
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL) return 0;
+        queue<TreeNode*> q;
+        int depth = 0;
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            while(size--){
+                TreeNode *node = q.front();
+                q.pop();
+                if(node->left != NULL) q.push(node->left);
+                if(node->right != NULL) q.push(node->right);
+            }
+            depth++;
+        }
+        return depth;
+    }
+};
