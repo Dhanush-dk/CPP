414b957cdf1bb8b8552e453d6d82346a7ab24510
Dhanushkumar <48850685+Dhanush-dk@users.noreply.github.com>
2022-01-09 11:31:28 +0530

    Updated

--- SDE-Sheet/Day3-Array/findDuplicate.cpp
+++ SDE-Sheet/Day3-Array/findDuplicate.cpp
@@ -28,3 +28,22 @@ public:
         return 0;
     }
 };
+
+
+//The following program returns duplicated elements from vectors in sorted order
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    vector<int> nums{1,2,3,4,3,5,4,2,3,5};
+    set<int> s;
+    unordered_map<int, int> map;
+    for(int i = 0; i < nums.size(); i++){
+        map[nums[i]]++;
+        if(map[nums[i]] > 1) s.insert(nums[i]);
+    }
+    for(auto i : s){
+        cout << i << " ";
+    }
+    return 0;
+}
+// The output is 2 3 4 5
