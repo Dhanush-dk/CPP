faab0a5313e4c8339d888892d77796e288090054
Dhanushkumar <48850685+Dhanush-dk@users.noreply.github.com>
2022-01-09 18:36:39 +0530

    Created

---
+++ Sorting/selectionSort.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i < n-1; i++){
+            int min = i;
+            for(int j = i+1; j < n; j++)
+                if(nums[j] < nums[min]) min = j;
+            swap(nums[i], nums[min]);
+        }
+        return nums;
+    }
+};
