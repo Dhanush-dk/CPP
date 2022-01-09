67b17124ca1c501bbd90c00e86ed24da69cc1ffe
Dhanushkumar <48850685+Dhanush-dk@users.noreply.github.com>
2022-01-09 10:42:33 +0530

    Updated

--- SDE-Sheet/Day15-Strings/implementStrstr().cpp
+++ SDE-Sheet/Day15-Strings/implementStrstr().cpp
@@ -1,3 +1,4 @@
+//Solution 1
 class Solution {
 public:
     int strStr(string haystack, string needle) {
@@ -9,6 +10,24 @@ public:
                 if(haystack[i + j] != needle[j]) break;
             }
             if(j == needle.size()) return i;
+        }
+        return -1;
+    }
+};
+
+//Solution 2
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle.size() == 0) return 0;
+        if(haystack.size() < needle.size()) return -1;  
+        int n = needle.size();
+        for(int i = 0; i <= haystack.size()-n; i++)
+        {  
+            if(haystack[i] == needle[0]){
+                if(haystack.substr(i,n) == needle)
+                    return i;
+            }   
         }
         return -1;
     }
