095d96ac3397588ee841d40cb719e36acc6adedb
Dhanushkumar <48850685+Dhanush-dk@users.noreply.github.com>
2022-01-11 14:25:52 +0530

    Created

---
+++ SDE-Sheet/DP/climbingStairs.cpp
@@ -0,0 +1,16 @@
+// This problem is nothing but finding i'th Fibonacci number
+class Solution {
+public:
+    int findWays(int n, int dp[]){
+        if(n <= 1) 
+            return 1;
+        if(dp[n] != -1) 
+            return dp[n];
+        return dp[n] = findWays(n-1, dp) + findWays(n-2, dp);
+    }
+    int climbStairs(int n) {
+        int dp[n+1];
+        memset(dp, -1, sizeof(dp));
+        return findWays(n, dp);
+    }
+};
