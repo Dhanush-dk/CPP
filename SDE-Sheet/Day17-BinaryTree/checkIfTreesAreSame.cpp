c6ccb59188d034e1c686e3de2d5787b82dac9992
Dhanushkumar <48850685+Dhanush-dk@users.noreply.github.com>
2022-01-15 11:49:34 +0530

    Created

---
+++ SDE-Sheet/Day17-BinaryTree/checkIfTreesAreSame.cpp
@@ -0,0 +1,9 @@
+// Checking if two trees are identical or not
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL || q == NULL)
+            return p==q;
+        return (p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
